dd19f747e0b3690fe46695f511a88a9e2aa6886a
116_Lv <lbw0109@gmail.com>
2026-03-24 17:29:28 +0900

    스텝2 완성

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,85 @@
-public class Calculator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Calculator {
+    private int num1;
+    private String oper;
+    private int num2;
+    private int result;
+
+    private ArrayList<Calculator> Lists = new ArrayList<>();
+
+    public Calculator() {}
+
+    public int calculate(int num1, String oper, int num2) {
+
+        // 연산자에 따른 계산처리
+        switch(oper) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+                result = -9999;
+        }
+        if(result != -9999) {
+            Calculator calculator = new Calculator();
+            calculator.setNum1(num1);
+            calculator.setOper(oper);
+            calculator.setNum2(num2);
+            calculator.setResult(result);
+            Lists.add(calculator);
+        }
+        return result;
+    }
+
+    public int getNum1() {
+        return this.num1;
+    }
+
+    public String getOper() {
+        return this.oper;
+    }
+
+    public int getNum2() {
+        return this.num2;
+    }
+
+    public int getResult() {
+        return this.result;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setOper(String oper) {
+        this.oper = oper;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    private void setResult(int result) {
+        this.result = result;
+    }
+
+    public List<Calculator> getLists() {
+        return Lists;
+    }
+
+    public void removeFirst() {
+        if(!Lists.isEmpty()) {
+            Lists.remove(0);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // 필요한 변수 & 객체 선언 및 초기화
         int num1 = 0;
         int num2 = 0;
         String oper = null;
         int result = 0;
         boolean endPoint = false;
         boolean checkPoint = false;
-        String continueYn = null;
+        int continueYn = 0;
 
         Scanner scanner = new Scanner(System.in);
+        Calculator cal = new Calculator();
 
-        while(!endPoint) {
+        // 계산기 시작
+        do {
             System.out.println("=== 계산기 시작 ===");
 
-            //1번째 숫자 입력
+            // 1번째 숫자 입력
             System.out.print("첫번째 숫자를 입력해주세요: ");
             do {
                 try {
@@ -27,7 +31,6 @@ public class Main {
                         System.out.print("0 이상의 숫자를 입력해주세요.\n다시 입력해주세요: ");
                     } else {
                         checkPoint = true;
-                        break;
                     }
                 } catch(InputMismatchException e) {
                     System.out.print("잘못된 입력입니다.\n다시 입력해주세요: ");
@@ -36,21 +39,19 @@ public class Main {
             } while(!checkPoint);
             checkPoint = false;
 
-            //연산자 입력
+            // 연산자 입력
             System.out.print("연산자를 입력해주세요: ");
             do {
                 oper = scanner.next();
-
                 if(oper.equals("+") || oper.equals("-") || oper.equals("*") || oper.equals("/")) {
                     checkPoint = true;
-                    break;
                 } else {
-                    System.out.print("잘못된 입력입니다.\n다시 입력해주세요: ");
+                    System.out.print("연산자가 아닌 입력입니다.\n다시 입력해주세요: ");
                 }
             } while(!checkPoint);
             checkPoint = false;
 
-            //2번째 숫자 입력
+            // 2번째 숫자 입력
             System.out.print("두번째 숫자를 입력해주세요: ");
             do {
                 try {
@@ -61,7 +62,6 @@ public class Main {
                         System.out.print("나눗셈 연산에서 분모로 0은 입력 될 수 없습니다.\n다시 입력해주세요: ");
                     } else {
                         checkPoint = true;
-                        break;
                     }
                 } catch(InputMismatchException e) {
                     System.out.print("잘못된 입력입니다.\n다시 입력해주세요: ");
@@ -70,42 +70,51 @@ public class Main {
             } while(!checkPoint);
             checkPoint = false;
 
-            switch(oper) {
-                case "+":
-                    result = num1+num2;
-                    break;
-                case "-":
-                    result = num1-num2;
-                    break;
-                case "*":
-                    result = num1*num2;
-                    break;
-                case "/":
-                    result = num1/num2;
-                    break;
-                default:
-                    System.out.println("잘못된 입력입니다.");
-            }
+            //계산
+            result = cal.calculate(num1, oper, num2);
 
+            // 결과 출력
             System.out.println(num1 + " " + oper + " " + num2 + " = " + result);
 
-            while(true) {
-                System.out.print("다시 계산하시겠습니까?(yes or exit): ");
-                continueYn = scanner.next();
+            // 재계산 여부 확인
+            do {
+                System.out.print("\n다음 메뉴 중 고르세요\n1. 기록조회\n2. 오래된 기록삭제\n3. 다시 계산\n4. 계산기 종료\n입력: ");
+                continueYn = scanner.nextInt();
 
-                if(continueYn.equalsIgnoreCase("exit")) {
-                    System.out.println("=== 계산기 종료 ===");
-                    endPoint = true;
-                    break;
-                } else if(continueYn.equals("yes")) {
-                    System.out.println("다시 계산합니다.\n");
-                    break;
-                } else {
-                    System.out.println("잘못된 입력입니다.");
+                switch (continueYn) {
+                    case 1:
+                        if(!cal.getLists().isEmpty()) {
+                            System.out.println("\n=== 계산 기록 ===");
+                            List<Calculator> list = cal.getLists();
+                            for(Calculator c : list) {
+                                System.out.println(c.getNum1() + " " + c.getOper() + " " + c.getNum2() + " = " + c.getResult());
+                            }
+                            System.out.println("=== 끝 ===");
+                        } else {
+                            System.out.println("현재 작성된 기록이 없습니다.\n");
+                        }
+                        break;
+                    case 2:
+                        cal.removeFirst();
+                        System.out.println("삭제되었습니다.\n");
+                        break;
+                    case 3:
+                        System.out.println("다시 계산합니다.\n");
+                        checkPoint = true;
+                        break;
+                    case 4:
+                        System.out.println("=== 계산기 종료 ===");
+                        endPoint = true;
+                        checkPoint = true;
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다.");
+                        break;
                 }
-            }
+            } while(!checkPoint);
+            checkPoint = false;
 
-        }
+        } while(!endPoint);
 
         scanner.close();
 
